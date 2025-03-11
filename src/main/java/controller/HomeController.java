f88bda7361779d6b248dffbde311bdcc14211313
shuresm57 <vsorensen31@gmail.com>
2025-03-11 18:57:09 +0100

    3rd commit

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -10,43 +10,53 @@ import org.springframework.web.bind.annotation.GetMapping;
 @ComponentScan()
 public class HomeController {
 
-    @GetMapping("/index")
-    public String index(){
-        return "index";
+    @GetMapping("/")
+    public String homeRedirect() {
+        return "redirect:/home/index";
     }
 
-    @GetMapping("/lion")
+    @GetMapping("/home/index")
+    public String index(){
+        return "home/index";
+    }
+
+    @GetMapping("/about/about")
+    public String about(){
+        return "about/about";
+    }
+
+    @GetMapping("/animals/lion")
     public String lionPage(Model model) {
         Lion lion = new Lion();
         model.addAttribute("lion", lion);
-        return "lion";
+        return "animals/lion";
     }
 
-    @GetMapping("/dolphin")
+    @GetMapping("/animals/dolphin")
     public String dolphinPage(Model model) {
         Dolphin dolphin = new Dolphin();
         model.addAttribute("dolphin", dolphin);
-        return "dolphin";
+        return "animals/dolphin";
     }
 
-    @GetMapping("/moose")
+    @GetMapping("/animals/moose")
     public String moosePage(Model model) {
         Moose moose = new Moose();
         model.addAttribute("moose", moose);
-        return "moose";
+        return "animals/moose";
     }
 
-    @GetMapping("/crab")
+    @GetMapping("/animals/crab")
     public String crabPage(Model model) {
         Crab crab = new Crab();
         model.addAttribute("crab", crab);
-        return "crab";
+        return "animals/crab";
     }
 
-    @GetMapping("/bear")
+    @GetMapping("/animals/bear")
     public String bearPage(Model model) {
         Bear bear = new Bear();
         model.addAttribute("bear", bear);
-        return "bear";
+        return "animals/bear";
     }
 }
